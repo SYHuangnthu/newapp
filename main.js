bc58c3b26aec8658a69dc817f395ae0d90aea85d
binbinko <43368507+binbinko@users.noreply.github.com>
2018-12-11 15:23:28 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -29,7 +29,10 @@ var conversationLog = new ReactiveVar("ELIZA: How are you doing?");
 Session.setDefault("currentPage", "frontPage");
 Session.setDefault("userSession", "");
 
-Meteor.subscribe("userConversation", Session.get("userSession"));
+Tracker.autorun(function() {
+  Meteor.subscribe("userConversation", Session.get("userSession"));
+});
+
 Template.body.helpers({
   checkCurrentPage: function(page) {
     return Session.equals("currentPage", page);
@@ -55,48 +58,41 @@ Template.formSection.events({
     event.preventDefault();
     let myMsgObj = document.getElementById("myMsg");
     let myMsg = myMsgObj.value;
-    Meteor.call("msgReceiver", myMsg, function(error, result){
-    if(error){
+    Meteor.call("msgReceiver", myMsg, Session.get("userSession"), function(error, result) {
+      if(error) {
 
-    }
-    else if(result === "Full!!"){
-    	alert("The database is full!");
-    }
-    else{
-    	
-    }
-  }),
-  myMsgObj.value = "";
-},
+      }
+      else if(result === "full") {
+        alert("The database is full!");
+      }
+      else {
+
+      }
+    });
+    myMsgObj.value = "";
+  },
   "click #resetMsg": function() {
-     //conversationLog.set("ELIZA: How are you doing?");
-     Meteor.call("resetMsg");
+    //conversationLog.set("ELIZA: How are you doing?");
+    Meteor.call("resetMsg", Session.get("userSession"));
+  },
+  "click #resetUser": function() {
+    Session.set("userSession", "");
+    Session.set("currentPage", "frontPage");
   }
 });
 
 Template.frontPage.events({
   "click #enterMain": function() {
-  	let username = document.getElementById("username").value;
-  	Meteor.call("setUser", username, function(error, result){
-  		if(error){
-  			alert("Username cannot have any space!")
-  		}
-  		else{
-  			Session.set("userSession", username);
- 		 	Session.set("currentPage", "home");
-  		}
-  	});
+    let username = document.getElementById("username").value;
+    Meteor.call("setUser", username, function(error, result) {
+      if(error) {
+        alert("Username cannot have any space!");
+      }
+      else {
+        Session.set("userSession", username);
+        Session.set("currentPage", "home");
+      }
+    });
   }
 });
-
-
-
-
-
-
-
-
-
-
-
 
